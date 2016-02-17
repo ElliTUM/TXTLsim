d0c47c8c7a611ea242cb82a5a9e1847054e163a1
ElliTum <elisabeth.falgenhauer@tum.de>
2016-02-17 15:49:14 +0100

    "neu"

--- Fitfkt_T2.m
+++ Fitfkt_T2.m
@@ -1,4 +1,4 @@
-function sol=Fitfkt_T2(param,c_0,data)
+function sol=Fitfkt_T2(param,c_0,data,errW,cutFFT)
 
 global bestparam besterr
 
@@ -13,5 +13,5 @@ lb=[bm{1,1} bm{1,2} bm{1,3} bm{1,4} bm{1,5} bm{1,6} bm{1,7} bm{1,8} bm{1,9} bm{1
 ub=[bm{2,1} bm{2,2} bm{2,3} bm{2,4} bm{2,5} bm{2,6} bm{2,7} bm{2,8} bm{2,9} bm{2,10} bm{2,11} bm{2,12} bm{2,13} bm{2,14} bm{2,15} bm{2,16}];
 
 options=optimset('MaxFunEvals',inf,'MaxIter',500);
-[nparam,fval,exitflag,output]=fmincon(@(param)ode23s_solver_T2(param,c_0,data),bestparam,[],[],[],[],lb,ub,[],options);
+[nparam,fval,exitflag,output]=fmincon(@(param)ode23s_solver_T2(param,c_0,data,errW,cutFFT),bestparam,[],[],[],[],lb,ub,[],options);
 output
